126c6d893f5865ec86cf653e5d744581e4fa348c
MrOkun <74727162+MrOkun@users.noreply.github.com>
2021-09-11 15:22:42 +0300

    Добавьте файлы проекта.

---
+++ Snow1/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Snow1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.Canvas_Box = new System.Windows.Forms.PictureBox();
+            this.TimerUpdate = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas_Box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas_Box
+            // 
+            this.Canvas_Box.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas_Box.Location = new System.Drawing.Point(0, 0);
+            this.Canvas_Box.Name = "Canvas_Box";
+            this.Canvas_Box.Size = new System.Drawing.Size(573, 561);
+            this.Canvas_Box.TabIndex = 0;
+            this.Canvas_Box.TabStop = false;
+            this.Canvas_Box.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Box_Paint);
+            // 
+            // TimerUpdate
+            // 
+            this.TimerUpdate.Enabled = true;
+            this.TimerUpdate.Interval = 33;
+            this.TimerUpdate.Tick += new System.EventHandler(this.TimerUpdate_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(573, 561);
+            this.Controls.Add(this.Canvas_Box);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Okun\'s Snow";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas_Box)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Canvas_Box;
+        private System.Windows.Forms.Timer TimerUpdate;
+    }
+}
+
---
+++ Snow1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snow1
+{
+    public partial class FormMain : Form
+    {
+        public struct Snowflake
+        {
+            public Snowflake(float x, float y, float speed, float size, float time)
+            {
+                this.X = x;
+                this.Y = y;
+                this.Speed = speed;
+                this.Size = size;
+                this.Time = time;
+            }
+
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float Speed { get; set; }
+            public float Size { get; set; }
+            public float Time { get; set; }
+        }
+
+        Graphics Canvas;
+        Pen _pen;
+        Random rnd;
+        Bitmap Back;
+
+        Snowflake[] Snowflacks;
+
+        int UpdateCount = 0;
+        readonly int SnowflakeCount = 120;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            Start();
+        }
+
+        private void MakeSnowflake()
+        {
+            for (int i = 0; i < SnowflakeCount; i++)
+            {
+                float addSpeed = 3 + (float)rnd.NextDouble() + (float)rnd.NextDouble() + (float)rnd.NextDouble();
+                Snowflacks[i] = new Snowflake(rnd.Next(0, 580), rnd.Next(0, 580), addSpeed, rnd.Next(8, 12), 1);
+            }
+        }
+
+        private void Start()
+        {
+            rnd = new Random();
+
+            Back = new Bitmap("backB.png", true);
+
+            //Canvas = Canvas_Box.CreateGraphics();
+
+            Bitmap myBitmap = new Bitmap(Width, Height);
+
+            Canvas_Box.Image = myBitmap;
+
+            Canvas = Graphics.FromImage(Canvas_Box.Image);
+            Snowflacks = new Snowflake[SnowflakeCount];
+            MakeSnowflake();
+        }
+
+        private void Canvas_Box_Paint(object sender, PaintEventArgs e)
+        {
+            this.Width = 573;
+            this.Height = 561;
+
+            SolidBrush SnowBrush = new SolidBrush(Color.LightGray);
+
+            //Canvas.Clear(Color.Black);
+            Canvas.DrawImage(Back, -240, 0);
+
+            for (int i = 0; i < SnowflakeCount; i++)
+            {
+                Canvas.FillEllipse(SnowBrush, Snowflacks[i].X, Snowflacks[i].Y, Snowflacks[i].Size, Snowflacks[i].Size);
+                
+                if (Snowflacks[i].Y > 600)
+                {
+                    Snowflacks[i].Y = 0;
+                    Snowflacks[i].Time = 0;
+                }
+                if (Snowflacks[i].X > 580 & Snowflacks[i].X < -5)
+                {
+                    Snowflacks[i].X = rnd.Next(0, 580);
+                }
+                else
+                {
+                    Snowflacks[i].Y += Snowflacks[i].Speed;
+                }
+            }
+
+        }
+
+        private void TimerUpdate_Tick(object sender, EventArgs e)
+        {
+            //this.InvokePaint(Canvas_Box, new PaintEventArgs(Canvas, new Rectangle(0, 0, Form.ActiveForm.Width, Form.ActiveForm.Height)));
+            Canvas_Box.Invalidate();
+        }
+    }
+}
